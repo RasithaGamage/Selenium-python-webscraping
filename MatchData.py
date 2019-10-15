a08ac7bf2b130b5076986b09cc393a4e39825417
Rasitha Gamage <g.rasitha@gmail.com>
2019-10-15 13:59:12 +0530

    Batting and bowling data saced to seperate files

--- MatchData.py
+++ MatchData.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 
 class MatchData:
     match_id =""
@@ -7,15 +5,15 @@ class MatchData:
     mom = ""
     match_result=""
     match_win = ""
+    ground_link = ""
+    match_link = ""
 
-    def __init__(self, match_id, match_title, mom, match_result, match_win):
+    def __init__(self, match_id, match_title, ground_link, match_link, mom, match_result, match_win):
         self.match_id = match_id
+        self.match_link = match_link
+        self.ground_link = ground_link
         self.match_title = match_title
         self.mom = mom
         self.match_result = match_result
         self.match_win = match_win
-
-    first_inning = ""
-
-    second_inning = ""
 
