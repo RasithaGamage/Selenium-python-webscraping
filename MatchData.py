b70972a142dad66879825be036917bda7f1f3e52
Rasitha Gamage <g.rasitha@gmail.com>
2019-10-21 00:36:45 +0530

    autmatically takes all data in a given year

--- MatchData.py
+++ MatchData.py
@@ -1,11 +1,8 @@
 import csv
-import csv
-from telnetlib import EC
+import traceback
+
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-
 from BatsmanData import BatsmanData
 from BowlerData import BowlerData
 
@@ -21,35 +18,58 @@ class MatchData:
 
         driver.get(self.url)
 
-        match_id = driver.find_element_by_xpath("""//*[@id="ciHomeContentlhs"]/div[3]/div/table[1]/tbody/tr[2]/td[7]""")
-        self.match_ground_link = driver.find_element_by_xpath("""//*[@id="ciHomeContentlhs"]/div[3]/div/table[1]/tbody/tr[2]/td[5]/a""").get_attribute("href")
-        self.match_link = match_id.find_element_by_tag_name("a").get_attribute("href")
-        self.match_id_val = match_id.text;
-        # print(match_id.text)
-        match_id.click()
-        match_main = driver.find_elements_by_class_name("gameHeader")
+        matches_table = driver.find_element_by_xpath("""//*[@id="ciHomeContentlhs"]/div[3]/div/table[1]/tbody""")
+        tr_matches = matches_table.find_elements_by_tag_name("tr")
+        for tr_match in tr_matches:
+            match_id_1 = tr_match.find_elements_by_tag_name("td")
+            match_id = match_id_1[6]
+            self.match_id_val = match_id_1[6].text
+            self.match_link = match_id_1[6].find_element_by_tag_name("a").get_attribute("href")
+            self.match_ground_link = match_id_1[4].find_element_by_tag_name("a").get_attribute("href")
 
-        for post in match_main:
-            if post.text != "":
-                self.info_overview = post.find_element_by_class_name("cscore_info-overview").text
-                # print("##########################"+info_overview)
-                self.player_of_match = post.find_element_by_class_name(
-                    "gp__cricket__player-match__player__detail__link").text
-                # print(player_of_match)
-                self.winning_team = post.find_element_by_class_name("cscore_notes_game").text
-                # print(winning_team)
-                scores = post.find_elements_by_class_name("cscore_team")
-                self.inn_1_score = scores[0].text
-                self.inn_2_score = scores[1].text
+            main_window = driver.current_window_handle
+            str_script = "window.open('"+self.match_link+"');"
+            driver.execute_script(str_script)
+            driver.switch_to.window(driver.window_handles[1])
 
+            match_main = driver.find_elements_by_class_name("gameHeader")
 
-        first_inning = driver.find_element_by_id("gp-inning-00")  # First inning data
-        self.getInningData(first_inning)
-        second_inning = driver.find_element_by_id("gp-inning-01")  # Second inning data
-        self.getInningData(second_inning)
+            for post in match_main:
+                if post.text != "":
+                    try:
+                     self.info_overview = post.find_element_by_class_name("cscore_info-overview").text
+                    except NoSuchElementException:
+                        traceback.print_exc(file=open("errlog.txt", "a"))
+                    try:
+                        self.player_of_match = post.find_element_by_class_name("gp__cricket__player-match__player__detail__link").text
+                    except NoSuchElementException:
+                        traceback.print_exc(file=open("errlog.txt", "a"))
 
+                    try:
+                        self.winning_team = post.find_element_by_class_name("cscore_notes_game").text
+                    except NoSuchElementException:
+                        traceback.print_exc(file=open("errlog.txt", "a"))
+                    try:
+                        scores = post.find_elements_by_class_name("cscore_team")
+                        self.inn_1_score = scores[0].text
+                        self.inn_2_score = scores[1].text
+                    except NoSuchElementException:
+                        traceback.print_exc(file=open("errlog.txt", "a"))
 
-        driver.back()
+            self.saveRecord()
+            try:
+                first_inning = driver.find_element_by_id("gp-inning-00")  # First inning data
+                self.getInningData(first_inning)
+            except NoSuchElementException:
+                traceback.print_exc(file=open("errlog.txt", "a"))
+            try:
+                second_inning = driver.find_element_by_id("gp-inning-01")  # Second inning data
+                self.getInningData(second_inning)
+            except NoSuchElementException:
+                traceback.print_exc(file=open("errlog.txt", "a"))
+
+            driver.close()
+            driver.switch_to.window(main_window)
 
     def saveRecord(self):
         with open('./CricMatchData.csv', 'a', newline='', encoding='utf-8') as outfile:
@@ -61,44 +81,69 @@ class MatchData:
     def getInningData(self, inning):
         batsmen_list = inning.find_element_by_class_name("scorecard-section")  # for batsmens
         batsmen_score = batsmen_list.find_elements_by_class_name("flex-row")
+
+        try:
+            score_headers = batsmen_score[0].find_element_by_class_name("header").find_elements_by_class_name("runs")
+
+        except NoSuchElementException:
+            traceback.print_exc(file=open("errlog.txt", "a"))
+
         for post2 in batsmen_score:
             check_availability = ""
             try:
-                check_availability = post2.find_element_by_class_name("batsmen").find_element_by_class_name(
-                    "batsmen").text
-            except Exception:
-                None
+                check_availability = post2.find_element_by_class_name("batsmen").find_element_by_class_name("batsmen").text
+            except NoSuchElementException:
+                traceback.print_exc(file=open("errlog.txt", "a"))
+
+            player_name = "No_data"
+            player_link = "No_data"
+            runs = "No_data"
+            balls = "No_data"
+            minutes = "No_data"
+            _4s = "No_data"
+            _6s = "No_data"
+            strike_Rate = "No_data"
+            commentary = "No_data"
+            dismissed_by = "No_data"
 
             if len(check_availability) > 0:
                 try:
                     batsman = post2.find_element_by_class_name("batsmen")
-                    score_record = batsman.find_elements_by_class_name("runs")
                     player_name = batsman.find_element_by_class_name("batsmen").text
-                    player_link = batsman.find_element_by_class_name("batsmen").find_element_by_tag_name(
-                        "a").get_attribute('href')
-                    runs = score_record[0].text
-                    balls = score_record[1].text
-                    minutes = score_record[2].text
-                    _4s = score_record[3].text
-                    _6s = score_record[4].text
-                    strike_Rate = score_record[5].text
-                    # print(player_name+" "+player_link+" "+runs+" "+balls+" "+minutes+" "+_4s+" "+_6s+" "+strike_Rate)
+                    player_link = batsman.find_element_by_class_name("batsmen").find_element_by_tag_name("a").get_attribute('href')
+
+                    score_record = batsman.find_elements_by_class_name("runs")
+
+                    for i, record in enumerate(score_record):
+
+                        if score_headers[i].text == "R":
+                            runs = record.text
+                        if score_headers[i].text == "B":
+                            balls = record.text
+                        if score_headers[i].text == "M":
+                            minutes = record.text
+                        if score_headers[i].text == "4s":
+                            _4s = record.text
+                        if score_headers[i].text == "6s":
+                            _6s = record.text
+                        if score_headers[i].text == "SR":
+                            strike_Rate = record.text
+
                 except NoSuchElementException:
-                    None
+                    traceback.print_exc(file=open("errlog.txt", "a"))
 
                 try:
                     commentary1 = post2.find_element_by_class_name("commentary-content")
                     commentary = commentary1.get_attribute("textContent")
-                    # print(commentary)
                 except NoSuchElementException:
-                    None
+                    traceback.print_exc(file=open("errlog.txt", "a"))
 
                 try:
                     commentary2 = post2.find_element_by_class_name("commentary")  # for find not out batsman
                     dismissed_by = commentary2.get_attribute("textContent")
                     # print(dismissed_by)
                 except NoSuchElementException:
-                    None
+                    traceback.print_exc(file=open("errlog.txt", "a"))
 
                 batsman_obj = BatsmanData(self.match_id_val, player_name, player_link, runs, balls, minutes, _4s, _6s,
                                           strike_Rate, commentary, dismissed_by)
@@ -166,41 +211,3 @@ class MatchData:
                                         runs_bowler, wickets, econ, dot_balls, _4s_bowler, _6s_bowler, wides, no_balls)
                 bowler_obj.saveRecord()
                 del bowler_obj
-
-    # def getData(self):
-    #     chrome_path = r"C:\Users\Rasitha\Desktop\chromedriver.exe"
-    #     chromeOptions = webdriver.ChromeOptions()
-    #     prefs = {'profile.managed_default_content_settings.images': 2}
-    #     chromeOptions.add_experimental_option("prefs", prefs)
-    #     driver = webdriver.Chrome(executable_path=chrome_path, chrome_options=chromeOptions)
-    #
-    #     driver.get(self.url)
-    #
-    #     match_id = driver.find_element_by_xpath("""//*[@id="ciHomeContentlhs"]/div[3]/div/table[1]/tbody/tr[2]/td[7]""")
-    #     self.match_ground_link = driver.find_element_by_xpath("""//*[@id="ciHomeContentlhs"]/div[3]/div/table[1]/tbody/tr[2]/td[5]/a""").get_attribute("href")
-    #
-    #     self.match_link = match_id.find_element_by_tag_name("a").get_attribute("href")
-    #     self.match_id_val = match_id.text;
-    #     # print(match_id.text)
-    #     match_id.click()
-    #     match_main = driver.find_elements_by_class_name("gameHeader")
-    #
-    #     for post in match_main:
-    #         if post.text != "":
-    #             self.info_overview = post.find_element_by_class_name("cscore_info-overview").text
-    #             # print("##########################"+info_overview)
-    #             self.player_of_match = post.find_element_by_class_name(
-    #                 "gp__cricket__player-match__player__detail__link").text
-    #             # print(player_of_match)
-    #             self.winning_team = post.find_element_by_class_name("cscore_notes_game").text
-    #             # print(winning_team)
-    #             scores = post.find_elements_by_class_name("cscore_team")
-    #             self.inn_1_score = scores[0].text
-    #             self.inn_2_score = scores[1].text
-    #
-    #
-    #     first_inning = driver.find_element_by_id("gp-inning-00")  # First inning data
-    #     # getInningData(first_inning)
-    #     second_inning = driver.find_element_by_id("gp-inning-01")  # Second inning data
-    #     # getInningData(second_inning)
-    #     driver.back()
--- __main__.py
+++ __main__.py
@@ -14,7 +14,6 @@ from BowlerData import BowlerData
 # change id=2010 into desired year to get required data
 
 match = MatchData("http://stats.espncricinfo.com/ci/engine/records/team/match_results.html?class=2;id=2010;team=8;type=year")
-match.saveRecord()
 
 del match
 
